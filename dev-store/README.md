93fad01e592246e756fcc472aa05300c41484e35
Vinicius Avila <viniciusvaln10@hotmail.com>
2024-10-04 18:24:03 -0300

    Listing products from the api

--- dev-store/README.md
+++ dev-store/README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Online Store Product Listing
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Overview
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+This project is a simple web application that consumes a RESTful API to retrieve and display a list of products from an online store. The application provides users with an intuitive interface to browse available products and view its details.
--- dev-store/src/pages/listProducts/index.jsx
+++ dev-store/src/pages/listProducts/index.jsx
@@ -1,11 +1,44 @@
 import styles from './listProducts.module.css'
+import { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 
+// criando o componente
 export const ListProducts = () => {
+
+    const [data,setData] = useState([])
+
+    // função responsável pela requisição
+    const listDataProducts = async () => {
+        const res = await fetch('https://api.escuelajs.co/api/v1/products');
+        const response = await res.json()
+
+        setData(response)
+        console.log(res)
+    }
+
+    // fazer a requisição logo quando a página é carregada e o componente montado
+    useEffect(()=> {
+        listDataProducts();
+    }, [])
+
     return (
         <>
-            <h3>
-                Listar Produtos
-            </h3>
+            <div className={styles.container}>
+                <div className={styles.card}>
+                    {data.map((item, index) => {
+                        return (
+                            <div className={styles.listProduct} key={index}>
+                               <Link className={styles.link}>
+                               <img src={item.category.image} alt='' width={200}/>
+                               <h2>{item.title}</h2>
+                               <br/>
+                               <button className={styles.btn}>Mais informações</button>
+                               </Link> 
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
