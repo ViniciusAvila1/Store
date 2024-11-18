0ab1b51f7144aa2dfeaeaf69d251ed81b5c8d3b3
Vinicius Avila <viniciusvaln10@hotmail.com>
2024-11-18 04:32:04 -0300

    feat: add React Icons package and implement back icon in product details

--- dev-store/src/pages/productsID/index.jsx
+++ dev-store/src/pages/productsID/index.jsx
@@ -1,34 +1,53 @@
-import styles from './productsID.module.css'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import styles from "./productsID.module.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { TiArrowBack } from "react-icons/ti";
+import { Link } from "react-router-dom";
 
 export const ProductsID = () => {
+  const [itemID, setItemID] = useState({});
+  const [loaded, setLoaded] = useState(false);
+  const { id } = useParams();
 
-    const [itemID, setItemID] = useState([])
-    const {id} = useParams()
-
-    const getItemId = async () => {
-        const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`)
-        const response = await res.json()
-        setItemID(response)
+  const getItemId = async () => {
+    setLoaded(true);
+    try {
+      const res = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`);
+      const response = await res.json();
+      setItemID(response);
+    } catch (error) {
+      console.error("Error searching for product:", error);
+    } finally {
+      setLoaded(false);
     }
+  };
 
-    useEffect(() => {
-        getItemId()
-    })
+  useEffect(() => {
+    getItemId();
+  }, [id]);
 
-    return (
-        <>
-            <h3>
-                Listar item específico
-            </h3>
-            <div>
-                {/* não precisa do map pois quero apenas um único item, não precisa percorrer tudo */}
-                <img src={itemID.images} alt='' width={200} />
-                <h2>{itemID.title}</h2>
-                <strong>R$: {itemID.price}</strong>
-                <p>{itemID.description}</p>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      {loaded && <h3>Hold on...</h3>}
+      {itemID && itemID.images ? (
+        <ul className={styles.ul}>
+          <div className={styles.info}>
+            <img src={itemID.images[0]} alt={itemID.title} />
+            <h2>Product: {itemID.title}</h2>
+            <strong>Price: {itemID.price}</strong>
+            <h3>Category: {itemID.category.name}</h3>
+            <p>
+              <strong>Description:</strong> {itemID.description}
+            </p>
+            <Link title="Back to products list" to={"/products"}>
+              <TiArrowBack />
+            </Link>
+          </div>
+        </ul>
+      ) : (
+        // tratamento de erro
+        !loaded && <h3>Product not found.</h3>
+      )}
+    </>
+  );
+};
